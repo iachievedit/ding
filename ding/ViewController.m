0875954ad2fd10b3ca353d62c61174f3bff88d5d
Joe Bell <joeaintexas@gmail.com>
2014-01-19 10:38:29 -0600

    Initial Revision

--- ding/ViewController.m
+++ ding/ViewController.m
@@ -3,12 +3,21 @@
 //  ding
 //
 //  Created by Joseph Bell on 1/19/14.
-//  Copyright (c) 2014 iAchieved.it LLC. All rights reserved.
+//  Copyright (c) 2014 iAchieved.it LLC.
+//
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
-@interface ViewController ()
+//
+#define CORRECT_IMPLEMENTATION
+
+@interface ViewController () <AVAudioPlayerDelegate>
+
+#ifdef CORRECT_IMPLEMENTATION
+  @property (nonatomic, strong) AVAudioPlayer* player;
+#endif
 
 @end
 
@@ -24,6 +33,30 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)playSound:(id)sender {
+  
+  NSLog(@"playSound");
+  
+  NSString* path = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"ding.caf"];
+  NSError* error;
+  
+#ifdef CORRECT_IMPLEMENTATION
+  self.player = [[AVAudioPlayer alloc] initWithContentsOfURL:[NSURL fileURLWithPath:path] error:&error];
+  self.player.delegate = self;
+  [self.player play];
+#else
+  AVAudioPlayer* player = [[AVAudioPlayer alloc] initWithContentsOfURL:[NSURL fileURLWithPath:path] error:&error];
+  [player play];
+#endif
+  
+}
+
+- (void)audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag {
+  if (flag) {
+    NSLog(@"audioPlayerDidFinishPlaying successfully");
+  }
 }
 
 @end
